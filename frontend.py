579de597bbed072de9b3451a896a0e74e3209b0c
Sarb0Z <93070907+Sarb0Z@users.noreply.github.com>
2023-05-14 18:32:41 +0500

    receiving file working

--- frontend.py
+++ frontend.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
 import pandas as pd
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/", methods=['GET'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+@app.route("/", methods=['GET', 'POST'])
 def main_page(name=None):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            df=pd.read_csv(filename)
+            print(df)
+            return redirect(url_for('download_file', name=filename))
     return render_template('index.html', name=name)
-# `read-form` endpoint 
-@app.route('/read-form', methods=['POST'])
-def read_form():
-  
-    # Get the form data as Python ImmutableDict datatype 
-    data = request.form
-  
-    ## Return the extracted information 
-    return {
-        'emailId'     : data['userEmail'],
-        'phoneNumber' : data['userContact'],
-        'password'    : data['userPassword'],
-        'gender'      : 'Male' if data['genderMale'] else 'Female',
-    }
-  
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
